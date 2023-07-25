2975757160d516cb6eca67377fd335db7551f815
Julio <julio.mech@hotmail.com>
2023-07-25 10:28:44 -0300

    alternative method

--- src/main/java/br/com/projectgptapi/gptapi/controllers/ChatController.java
+++ src/main/java/br/com/projectgptapi/gptapi/controllers/ChatController.java
@@ -1,5 +1,36 @@
 package br.com.projectgptapi.gptapi.controllers;
 
-public class ChatController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import br.com.projectgptapi.gptapi.dto.ChatRequest;
+import br.com.projectgptapi.gptapi.dto.ChatResponse;
+
+@RestController
+public class ChatController {
+    @Qualifier("openaiRestTemplate")
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${openai.model}")
+    private String model;
+    @Value("${openai.api.url}")
+    private String apiUrl;
+
+    public String chat(@RequestParam String prompt) {
+
+        ChatRequest request = new ChatRequest(model, prompt);
+
+        ChatResponse response = restTemplate.postForObject(apiUrl, request, ChatResponse.class);
+
+        if (response == null || response.getChoices() == null || response.getChoices().isEmpty()) {
+            return "No response";
+        } else {
+            return response.getChoices().get(0).getMessage().getContent();
+        }
+    }
 }
