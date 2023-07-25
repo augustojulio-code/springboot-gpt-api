a6a744e4aa775d50bccbf22cbed48ff566619bd6
Julio <julio.mech@hotmail.com>
2023-07-25 10:39:16 -0300

    alternate method

--- src/main/java/br/com/projectgptapi/gptapi/controllers/ChatController.java
+++ src/main/java/br/com/projectgptapi/gptapi/controllers/ChatController.java
@@ -3,6 +3,7 @@ package br.com.projectgptapi.gptapi.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -21,6 +22,7 @@ public class ChatController {
     @Value("${openai.api.url}")
     private String apiUrl;
 
+    @GetMapping("/chat")
     public String chat(@RequestParam String prompt) {
 
         ChatRequest request = new ChatRequest(model, prompt);
